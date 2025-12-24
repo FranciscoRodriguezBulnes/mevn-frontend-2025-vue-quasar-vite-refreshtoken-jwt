59aedaa2af0d45677658675c4c2d17cc5ac20509
Francisco Rodríguez Bulnes <158209260+FranciscoRodriguezBulnes@users.noreply.github.com>
2025-12-24 05:48:13 +0100

    ✔

--- quasar.config.js
+++ quasar.config.js
@@ -19,7 +19,7 @@ export default defineConfig((/* ctx */) => {
     // https://github.com/quasarframework/quasar/tree/dev/extras
     extras: [
       // 'ionicons-v4',
-      // 'mdi-v7',
+       'mdi-v7',
       // 'fontawesome-v6',
       // 'eva-icons',
       // 'themify',
@@ -37,7 +37,7 @@ export default defineConfig((/* ctx */) => {
         node: 'node20',
       },
 
-      vueRouterMode: 'hash', // available values: 'hash', 'history'
+      vueRouterMode: 'history', // available values: 'hash', 'history'
       // vueRouterBase,
       // vueDevtools,
       // vueOptionsAPI: false,
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,13 +1,90 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    />
+  <q-page padding>
+    <q-btn @click="access">Acceder</q-btn>
+    <q-btn @click="createLink">Crear Link</q-btn>
+    {{ token }} - {{ expiresIn }}
   </q-page>
 </template>
 
 <script setup>
-//
+// import axios from 'axios'
+import { api } from 'src/boot/axios'
+import { ref } from 'vue'
+
+const token = ref('')
+const expiresIn = ref('')
+
+// const access = () => {
+//   console.log('has pulsado click')
+//   axios
+//     .post('http://localhost:5000/api/v1/auth/login', {
+//       email: 'frbacsa@gmail.com',
+//       password: 'jacobo1!',
+//     })
+//     .then((res) => {
+//       console.log(res.data)
+//     })
+//     .catch((e) => console.log(e))
+// }
+
+const access = async () => {
+  try {
+    console.log('has pulsado click')
+    const res = await api.post('/auth/login', {
+      email: 'frbacsa@gmail.com',
+      password: 'jacobo1!',
+    })
+    // console.log(res.data)
+    token.value = res.data.token
+    expiresIn.value = res.data.expiresIn
+    setTime()
+    // console.log(token)
+    // console.log(expiresIn)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const createLink = async () => {
+  try {
+    const res = await api({
+      method: 'POST',
+      url: '/links',
+      headers: {
+        Authorization: 'Bearer ' + token.value,
+      },
+      data: {
+        longLink:
+          'https://www.google.com/search?q=calzoncillos+con+relleno+gl%C3%BAteos+hombre&rlz=1C1GCEA_enES989ES1046&oq=&gs_lcrp=EgZjaHJvbWUqCQgBECMYJxjqAjIJCAAQIxgnGOoCMgkIARAjGCcY6gIyCQgCECMYJxjqAjIJCAMQIxgnGOoCMgkIBBAjGCcY6gIyCQgFECMYJxjqAjIJCAYQIxgnGOoCMgkIBxAjGCcY6gLSAQkxNjE0ajBqMTWoAgiwAgHxBZKQZb6It8hZ8QWSkGW-iLfIWQ&sourceid=chrome&ie=UTF-8',
+      },
+    })
+    console.log(res.data)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const setTime = () => {
+  setTimeout(
+    () => {
+      refreshToken()
+    },
+    expiresIn.value * 1000 - 6000,
+  )
+}
+
+const refreshToken = async () => {
+  try {
+    const res = await api.get('/auth/refresh')
+    console.log(res.data)
+
+    token.value = res.data.token
+    expiresIn.value = res.data.expiresIn
+    setTime()
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+// refreshToken()
 </script>
